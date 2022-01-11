0fca36972556ab9e51df962db569b84894b89588
cabarronc <cabarronc@guanajuato.gob.mx>
2022-01-11 15:32:50 -0600

    created area pruebas

---
+++ src/app/Views/Administracion/pruebaspdf/pruebaspdf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pruebaspdf',
+  templateUrl: './pruebaspdf.component.html',
+  styleUrls: ['./pruebaspdf.component.css']
+})
+export class PruebaspdfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
