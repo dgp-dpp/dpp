3195d7d626f359efa5936a989b791e0198b46d1f
RDCC <chorelandia@gmail.com>
2022-01-18 10:11:33 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 import { GridModule, PDFModule,ExcelModule } from '@progress/kendo-angular-grid';
-
+import { ChrtshighComponent } from './Views/Administracion/highcharts/chrtshigh.component';
 
 
 
